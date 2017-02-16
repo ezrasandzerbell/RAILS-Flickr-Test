3d117256c7ab837667b56033cf105fb35aa5e558
Ezra Sandzer-Bell and Josh Rinard <ezra+josh@>
2017-02-16 11:35:10 -0800

    simple cove integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,10 @@
 class HomeController < ApplicationController
 
-
-
   def index
     @photos = Photo.all
     # @user = current_user      # filtering by user
     # @photos = @user.photos
   end
 
-  def show
-    @photo = Photo.find(params[:id])
-  end
 
 end
